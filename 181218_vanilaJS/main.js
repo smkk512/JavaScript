78547d95aeeedbd095bc7ae333494c89d2487586
KSY <smkk512@naver.com>
2018-12-18 23:17:43 +0900

    commited Timer html

---
+++ 181218_vanilaJS/main.js
@@ -0,0 +1,30 @@
+
+const clockText = document.querySelector("#Clock");
+const clockClicked = 'ClockClicked';
+
+const getTime = () =>{
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return `${hours<10? '0'+hours:hours}:${minutes<10 ? '0'+minutes:minutes}:${seconds < 10 ? '0'+seconds:seconds}`;
+}
+
+const handleClick = () => {
+    const hasID = clockText.classList.contains(clockClicked);
+    if(hasID){
+        clockText.classList.remove(clockClicked);
+    } else {
+        clockText.classList.add(clockClicked);
+    }
+}
+
+const init = () =>{
+    clockText.addEventListener("click", handleClick);
+    setInterval(()=>{
+        clockText.innerHTML = getTime();
+    }, 1000);
+}
+
+init();
