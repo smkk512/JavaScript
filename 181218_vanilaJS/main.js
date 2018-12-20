4044b6ff37c70e4df1e00e0452d5a1f1225ce155
KSY <smkk512@naver.com>
2018-12-20 18:22:58 +0900

    1812201822 textfield

--- 181218_vanilaJS/main.js
+++ 181218_vanilaJS/main.js
@@ -1,5 +1,7 @@
 
 const clockText = document.querySelector("#Clock");
+const welcomeText = document.querySelector("#welcomeText")
+const inputName = document.querySelector("#inputName")
 const clockClicked = 'ClockClicked';
 
 const getTime = () =>{
@@ -9,6 +11,23 @@ const getTime = () =>{
     const seconds = date.getSeconds();
 
     return `${hours<10? '0'+hours:hours}:${minutes<10 ? '0'+minutes:minutes}:${seconds < 10 ? '0'+seconds:seconds}`;
+}
+
+const handleNameSubmit = (event) =>{
+    const key = event.which || event.keyCode;
+    if(key === 13){
+        const value = event.currentTarget.value;
+        setName(value);
+    }
+}
+
+const setName = (name) => {
+    const localName = localStorage.getItem('currentUser');
+    if(localName === null){
+        localStorage.setItem('currentUser', name);    
+    }
+    
+    welcomeText.innerHTML = "welcome "+localName;
 }
 
 const handleClick = () => {
@@ -21,7 +40,10 @@ const handleClick = () => {
 }
 
 const init = () =>{
+    clockText.innerHTML = getTime();
     clockText.addEventListener("click", handleClick);
+    inputName.addEventListener("keypress", handleNameSubmit);
+
     setInterval(()=>{
         clockText.innerHTML = getTime();
     }, 1000);
