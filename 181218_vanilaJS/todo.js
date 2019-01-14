4d558b4ad15995e892f5e0a617f5c20a1a15099a
KSY <smkk512@naver.com>
2019-01-14 23:29:50 +0900

    implement del todo

--- 181218_vanilaJS/todo.js
+++ 181218_vanilaJS/todo.js
@@ -5,8 +5,12 @@ const frmTodo = document.querySelector('.frm-todo'),
 
 const TODOS_LS = 'toDos';
 
+const arrToDos = []; // todo를 오브젝트에 담아 배열로 저장
+
 const handleDelBtnClick = event => {
-    console.log(event);
+    const target = event.toElement.parentNode;
+    const targetParent = event.toElement.parentNode.parentNode;
+    targetParent.removeChild(target);
 }
 
 //입력한 투두를 리스트에 출력하는 함수
@@ -16,9 +20,18 @@ const printTodo = value => {
     const span = document.createElement('span');
 
     delBtn.innerHTML = 'X';
-    span.innerText = value;
+    delBtn.addEventListener('click', handleDelBtnClick);
+    const objTodo = {
+        id:arrToDos.length +1,
+        text:value,
+    };
+    span.innerText = objTodo.text;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = 'todo_' + objTodo.id;
+    arrToDos.push(objTodo);
+    saveToDos();
+
     listTodo.appendChild(li);
 }
 
@@ -30,20 +43,25 @@ const handleSubmit = event => {
     inputTodo.value = "";
 }
 
+//todo배열을 localstorage에 저장하는 함수
+const saveToDos = () => {
+    localStorage.setItem(TODOS_LS, JSON.stringify(arrToDos));
+}
+
 //로컬스토리지에 저장된 투두리스트를 불러오는 함수
 const loadToDos = () => {
     const toDos = localStorage.getItem(TODOS_LS);
     if(toDos !== null){
-
+        const parsedToDos = JSON.parse(toDos);
+        parsedToDos.forEach(todo => {
+            printTodo(todo.text);
+        });
     }
 }
 
 const todoInit = () => {
     loadToDos();
     frmTodo.addEventListener('submit', handleSubmit);
-
-    const delBtn = listTodo.querySelector('button');
-    delBtn.addEventListener('click', handleDelBtnClick);
 }
 
 todoInit();
